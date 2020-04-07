dcc66bcc56b806ee76d494df219133b94c64e846
Josh Jones <ohblonddev@gmail.com>
2020-04-07 23:15:14 +0100

    move greeting text to Greet function

--- cmd/egp/main.go
+++ cmd/egp/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jjmark15/example-go-project/egp/pkg/greeting"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println(greeting.Hello())
 }

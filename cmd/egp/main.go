9592d1e6ed96d1b584f71726c4ec7fe278d984c4
Josh Jones <ohblonddev@gmail.com>
2020-04-07 23:24:45 +0100

    delegate hello world to greet me function

--- cmd/egp/main.go
+++ cmd/egp/main.go
@@ -7,5 +7,5 @@ import (
 )
 
 func main() {
-	fmt.Println(greeting.Hello())
+	fmt.Println(greeting.HelloWorld())
 }
--- pkg/greeting/greeting.go
+++ pkg/greeting/greeting.go
@@ -1,5 +1,15 @@
 package greeting
 
-func Hello() string {
-	return "Hello world!"
+import "strings"
+
+func HelloWorld() string {
+	return GreetMe("World")
+}
+
+func GreetMe(s string) string {
+	var str strings.Builder
+	str.WriteString("Hello ")
+	str.WriteString(s)
+	str.WriteString("!")
+	return str.String()
 }
